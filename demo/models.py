91cfb07e182a9e28e1989f3179d88ab8dac8c9ab
Sebastian Acuna <sacuna@gmail.com>
2011-11-10 19:54:59 -0800

    Delete related behavior for demo models

--- demo/models.py
+++ demo/models.py
@@ -1,28 +1,48 @@
 from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models.signals import pre_delete
+
 
 # Create your models here.
 
 class Model(models.Model):
-	# Allow customization for base behavior here
-	
-	class Meta:
-		abstract = True
+    # Allow customization for base behavior here
+    #deleted = models.BooleanField(default=False)
+    
+    def delete(self):
+        self.delete_related()
+        super(Model, self).delete()
+        
+    def delete_related(self):
+        pass
+
+    class Meta:
+        abstract = True
 
 # Model persons involved and openings available
 
 class Person(Model):
-	name = 			models.CharField(max_length=100)
-	surname = 		models.CharField(max_length=100)
-	email = 		models.EmailField()
-	date_of_birth = models.DateField(blank=True, null=True)
-	address =		models.TextField(blank=True)
-	
-	def __unicode__(self):
-		return "%s %s" % (self.name, self.surname)
+    name =          models.CharField(max_length=100)
+    surname =       models.CharField(max_length=100)
+    email =         models.EmailField()
+    date_of_birth = models.DateField(blank=True, null=True)
+    address =       models.TextField(blank=True)
+
+    def delete_related(self):
+        self.pet_set.clear()
+    
+    def __unicode__(self):
+        return "%s %s" % (self.name, self.surname)
 
 class Pet(Model):
-	name = 			models.CharField(max_length=100)
-	master =        models.ForeignKey(Person)
+    name =          models.CharField(max_length=100)
+    master =        models.ForeignKey(Person, default=None, on_delete=models.SET_NULL, blank=True, null=True)
 
-	def __unicode__(self):
-		return "%s %s" % (self.name)
+    def __unicode__(self):
+        return "%s" % (self.name)
+
+def delete_related(sender, **kwargs):
+    kwargs["instance"].delete_related()
+
+pre_delete.connect(delete_related, sender=Person)
+
